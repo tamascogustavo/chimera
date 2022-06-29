250f3321fa51b72383c777caf277425ac2366a1d
Gustavo Tamasco <tamascogustavo@gmail.com>
2022-06-29 08:45:22 -0300

    Config install files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+"""Set up the cobra package."""
+from setuptools import setup
+
+
+# All other arguments are defined in `setup.cfg`.
+if __name__ == "__main__":
+    setup(version="2.0.2")
